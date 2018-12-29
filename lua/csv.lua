008de523010147d04b2ee9eb803b28d72b8cac40
marcelvanherk <marcelvanherk@users.noreply.github.com>
2018-12-29 00:03:28 +0000

    Update csv.lua with fixes from UoM
    -- mvh/AM 20160329 Fix for using ulua
    -- mvh 20160907 Convert to number if possible, local f; support reading bool and nil
    -- mvh 20170524 Note: readCSV(file, false) does not return unstructured table
    -- mvh 20180105 Fix reading unstructured CSV file
    -- mvh+amw 20180209 Fix reading structured CSV file

--- lua/csv.lua
+++ lua/csv.lua
@@ -9,8 +9,17 @@
 
 -- mvh 20151126 Fix for tables with empty header cells
 -- mvh 20151130 Fix for tables with booleans
+-- mvh/AM 20160329 Fix for using ulua
+-- mvh 20160907 Convert to number if possible, local f; support reading bool and nil
+-- mvh 20170524 Note: readCSV(file, false) does not return unstructured table
+-- mvh 20180105 Fix reading unstructured CSV file
+-- mvh+amw 20180209 Fix reading structured CSV file
 
 function escapeCSV (s)
+  if s==nil or s=='nil' then
+    return nil
+  end
+  
   if type(s)~='string' then
     s = tostring(s)
   end
@@ -36,11 +45,16 @@ function fromCSV (s)
       until c ~= '"'    -- quote not followed by quote?
       if not i then error('unmatched "') end
       local f = string.sub(s, fieldstart+1, i-1)
+      if f=='nil' then f='' end
       table.insert(t, (string.gsub(f, '""', '"')))
       fieldstart = string.find(s, ',', i) + 1
     else                -- unquoted; find next comma
       local nexti = string.find(s, ',', fieldstart)
-      table.insert(t, string.sub(s, fieldstart, nexti-1) or '')
+      local f = string.sub(s, fieldstart, nexti-1) or ''
+      if f=='nil'   then f = '' end
+      if f=='TRUE'  then f = true end
+      if f=='FALSE' then f = false end
+      table.insert(t, f)
       fieldstart = nexti + 1
     end
   until fieldstart > string.len(s)
@@ -60,11 +74,12 @@ end
 -- returns table[1..nlines] of tables with items, 
 -- and returns table with ordered heading (needed to rewrite)
 function readCSV(filename, itemnames)
-  itemnames = itemnames or true
-  local t = {}, heads
-  f = io.open(filename, 'rt')
-  for s in f:lines(s) do 
-    if itemnames then
+  if itemnames==nil then itemnames=true end
+  local t = {}
+  local heads
+  local f = io.open(filename, 'rt')
+  for s in f:lines() do 
+    if itemnames==true then
       heads = fromCSV (s)
       for k,v in ipairs(heads) do 
         if v=='' then v = 'Column'..k end
@@ -76,7 +91,14 @@ function readCSV(filename, itemnames)
       s = fromCSV (s)
       for k,v in ipairs(s) do
         if v~='' then
-          t[#t][heads[k]] = v
+          if tonumber(v) then
+		    v = tonumber(v)
+		  end
+          if heads then
+            t[#t][heads[k]] = v
+          else
+            t[#t][k] = v
+          end
         end
       end
     end

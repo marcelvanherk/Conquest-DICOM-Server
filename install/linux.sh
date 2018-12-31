b4c33f7ed83e126141c714fbb5f6e9495d8a68c5
marcelvanherk <marcelvanherk@users.noreply.github.com>
2018-12-31 17:56:49 +0000

    Added (commented out) SeLinux line to web installer

--- install/linux.sh
+++ install/linux.sh
@@ -6,11 +6,14 @@
 # mvh 20181117 For 1.4.19c; add luasocket; copy of anonymize_script
 # mvh 20181115 For 1.4.19c; copy dgate.dic to install, use define for CGI
 # mvh 20181223 For 1.4.19d; Added luasocket; use sensible-browser
+# mvh 20181230 For 1.4.19d; Added setenforce line for Fedora
 
 SRC=../src/dgate;
 CGI=/usr/lib/cgi-bin; # /var/www/cgi-bin for Fedora
 
-sudo fuser -k 5912/tcp
+sudo fuser -k 5912/tcp;
+
+#sudo setenforce Permissive; # unblock for Fedora
 
 cp ../dgate.dic .
 mkdir lua

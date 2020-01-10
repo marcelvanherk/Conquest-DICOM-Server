ee790565a765a0fd1ae28735677d82319d0e334c
marcelvanherk <marcelvanherk@users.noreply.github.com>
2020-01-10 21:05:16 +0000

    Allow change of transfersyntax in servertask (as yet unused)
    // mvh 20200110; Allow change of transfer syntax

--- src/servertask/servertask.cxx
+++ src/servertask/servertask.cxx
@@ -1,5 +1,6 @@
 // Small echo DLL used to control the dicom server from the delphi GUI
 // mvh 20181215; based on code by ljz and lsp
+// mvh 20200110; Allow change of transfer syntax
 
 #include "dicom.hpp"
 
@@ -11,6 +12,25 @@ int gl_LastError;
 #define DCM_E_REMOTE 20
 #define DCM_E_OUTPUTFILE 21
 
+class	EPDU_Service	:
+	public		PDU_Service
+	{
+		BOOL	AddTransferSyntaxs(PresentationContext &);	// override original function
+	};
+
+BOOL	EPDU_Service	::	AddTransferSyntaxs(PresentationContext &PresContext)
+	{
+	UID		uid;
+	TransferSyntax	TrnSyntax;
+
+	uid.Set("1.2.840.10008.1.2");		// ImplicitLittleEndian
+	TrnSyntax.Set(uid);
+	PresContext.AddTransferSyntax(TrnSyntax);
+	return ( TRUE );
+	}
+
+RTC	VRType(TRUE);					// VR translation table
+	
 int WINAPI DcmEcho(	char* pszLocalAE,
 			char* pszRemoteAE,
 			char* pszRemoteIP,
@@ -19,7 +39,7 @@ int WINAPI DcmEcho(	char* pszLocalAE,
 			char* pszNKIcommand2)
 {
 	int			rc;
-	PDU_Service		PDU;
+	EPDU_Service		PDU;
 	DICOMCommandObject	DCO;
 	DICOMCommandObject	DCOR;
 	DICOMDataObject		DDO;
@@ -29,19 +49,27 @@ int WINAPI DcmEcho(	char* pszLocalAE,
 	LE_UINT16		command, datasettype, messageid;
 	BYTE			SOP[64];
 	TransferSyntax		TrnSyntax;
-
+	PresentationContext     PresContext;
+	
         gl_LastError = DCM_E_OK;
 
+        VRType.AttachRTC(".\\dgate.dic");
+	PDU.AttachRTC(&VRType);
 	PDU.ClearAbstractSyntaxs();
 	PDU.SetLocalAddress((unsigned char*)pszLocalAE);
 	PDU.SetRemoteAddress((unsigned char*)pszRemoteAE);
+	
+
+/*FILE *f; char text[64]; int typ;
+typ=VRType.RunTimeClass(0x9999, 0x0300, text);
+f = fopen("test.txt", "wb");
+fwrite(text, 1, 64, f);
+fprintf(f, "%c%c", typ/256, typ&255);
+fclose(f);*/
 
 	uid.Set("1.2.840.10008.3.1.1.1");	// Application context (DICOM App)
 	PDU.SetApplicationContext(uid);
 	
-	//uid.Set("1.2.840.10008.1.2.1"); TrnSyntax.Set(uid);
-	//PDU.AddTransferSyntax(TrnSyntax);
-
 	uid.Set("1.2.840.10008.1.1");	// Verification
 	PDU.AddAbstractSyntax(uid);
 
@@ -53,25 +81,29 @@ int WINAPI DcmEcho(	char* pszLocalAE,
 	vr = new VR (0x0000, 0x0002, strlen((char*)SOP), (void*) SOP, FALSE);
 	DCO.Push(vr);
 
+	command = 0x0030;
+	vr = new VR (0x0000, 0x0100, 0x0002, &command, FALSE);
+	DCO.Push(vr);
+	messageid = 7;
+	vr = new VR (0x0000, 0x0110, 0x0002, &messageid, FALSE);
+	DCO.Push(vr);
+	datasettype = 0x0101;
+	vr = new VR (0x0000, 0x0800, 0x0002, &datasettype, FALSE);
+	DCO.Push(vr);
+
 	if (pszNKIcommand1 && strlen(pszNKIcommand1) && (pszNKIcommand1[0] != '#'))
 	{ vr = new VR (0x9999, 0x0300, strlen((char*)pszNKIcommand1), (void*)pszNKIcommand1, FALSE);
+          vr->TypeCode='UN';
 	  DCO.Push(vr);
 	}
 
 	if (pszNKIcommand2 && strlen(pszNKIcommand2))
 	{ vr = new VR (0x9999, 0x0400, strlen((char*)pszNKIcommand2), (void*)pszNKIcommand2, FALSE);
+          vr->TypeCode='UN';
 	  DCO.Push(vr);
 	}
-	command = 0x0030;
-	vr = new VR (0x0000, 0x0100, 0x0002, &command, FALSE);
-	DCO.Push(vr);
-	datasettype = 0x0101;
-	vr = new VR (0x0000, 0x0800, 0x0002, &datasettype, FALSE);
-	DCO.Push(vr);
-	messageid = 7;
-	vr = new VR (0x0000, 0x0110, 0x0002, &messageid, FALSE);
-	DCO.Push(vr);
 
+	uid.Set("1.2.840.10008.1.1");	// Verification
 	PDU.Write(&DCO, uid);
 
 	if(!PDU.Read(&DCOR))

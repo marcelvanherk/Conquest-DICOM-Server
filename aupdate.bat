c661c325ccb272c6931df09e925981002fe9d672
marcelvanherk <marcelvanherk@users.noreply.github.com>
2021-06-20 13:39:48 +0100

    Update dictionary to support modern Philips MRI
    Conquest requires all sequences to be defined in dgate.dic, also private ones. The lack of a few new ones caused a failure to jpeg compress such new MRI images, likely because a temporary dicom file was not read correctly. Also changed script to create and update binary versions of dictionary in web server folders,
    
    # 20210620      mvh     Made 2005,1084; 2005,140F; 2005,1580 sequence to support modern Philips MR

--- aupdate.bat
+++ aupdate.bat
@@ -8,6 +8,9 @@ copy ..\doc\DicomConformance_FilesLST_Changes.pdf .
 copy ..\doc\linuxmanual.pdf .
 
 copy dgate.dic .\linux
+c:\dicom150beta\dgate64 --mk_binary_dic:t.dic
+copy c:\dicom150beta\t.dic .\webserver\cgi-bin\dgate.dic
+copy c:\dicom150beta\t.dic .\webserver\cgi-bin\newweb\dgate.dic
 
 del .\webserver\cgi-bin\dgate.exe
 del .\webserver\cgi-bin\newweb\dgate.exe

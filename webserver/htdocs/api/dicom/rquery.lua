a0e226dabaa6ceed5ca17b60924994de84e432f4
marcelvanherk <marcelvanherk@users.noreply.github.com>
2022-08-16 22:22:09 +0100

    Include (old version) of OHIF that runs on my PHP API
    To run say:
    
    npm install
    npm run
    
    dgate64 in the server and webserver/htdocs/api have to be up to date

--- webserver/htdocs/api/dicom/rquery.lua
+++ webserver/htdocs/api/dicom/rquery.lua
@@ -25,11 +25,45 @@ function remotemetadata(ae, level, st)
   q2.StudyInstanceUID=']]..st..[['
   q2.InstanceNumber=''
   q2.Rows=''
-  q2.Colums=''
+  q2.Columns=''
   q2.PatientID=''
   q2.PatientName=''
   q2.SOPInstanceUID=''
+  q2.NumberOfFrames=''
+  q2.BitsStored=''
+  q2.SamplesPerPixel=''
+  q2.SOPClassUID=''
+  q2.SliceLocation=''
+  q2.Modality=''
+  q2.FrameOfReferenceUID=''
+  q2.PatientPosition=''
+  q2.SeriesTime=''
+  q2.SeriesDate=''
+  q2.StudyTime=''
+  q2.StudyDate=''
+  q2.ImageType=''
   local r = dicomquery(ae, 'IMAGE', q2)
+  local d = DicomObject:new()
+  d:Read(':'..r[0].SOPInstanceUID)
+  for i=0, #r-1 do 
+    r[i].WindowCenter=d.WindowCenter 
+    r[i].WindowWidth=d.WindowWidth
+    r[i].RescaleIntercept=d.RescaleIntercept
+    r[i].RescaleSlope=d.RescaleSlope
+    r[i].HighBit=d.HighBit
+    r[i].PixelRepresentation=d.PixelRepresentation
+    r[i].BitsAllocated=d.BitsAllocated
+    r[i].PixelSpacing=d.PixelSpacing
+    r[i].PhotometricInterpretation = d.PhotometricInterpretation
+    r[i].ImageOrientationPatient = d.ImageOrientationPatient
+    r[i].ImagePositionPatient = d.ImagePositionPatient
+    r[i].SliceThickness = d.SliceThickness
+    r[i].PatientOrientation = d.PatientOrientation
+    r[i].ContrastBolusAgent = d.ContrastBolusAgent
+    r[i].AccessionNumber = d.AccessionNumber
+    r[i].ImageComments = d.ImageComments
+    r[i]:SetVR(0x8,0x1140,d:GetVR(0x8,0x1140))
+  end
   r = r:Serialize(true,false,true)
   local s=tempfile('txt') local f=io.open(s, "wb") f:write(r) returnfile=s f:close()
 ]]

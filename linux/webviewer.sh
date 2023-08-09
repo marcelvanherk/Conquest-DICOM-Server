c00203d43cd6d6751e8e5060c32b7d4d70b4ad72
marcelvanherk <marcelvanherk@users.noreply.github.com>
2023-08-09 09:23:35 +0100

    Linux webviewer script no longer stops ladle (would stop before used!)

--- linux/webviewer.sh
+++ linux/webviewer.sh
@@ -1,3 +1,3 @@
 ./dgate "--luastart:package.path=Global.basedir..[[lua/?.lua]];dofile(Global.basedir..[[lua/ladle.lua]])"
 sensible-browser 127.0.0.1:8086/app/newweb
-./dgate "--luastart:package.path=Global.basedir..[[lua/?.lua]];dofile(Global.basedir..[[lua/quitladle.lua]])"
+#./dgate "--luastart:package.path=Global.basedir..[[lua/?.lua]];dofile(Global.basedir..[[lua/quitladle.lua]])"

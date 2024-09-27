11303435ef2ec898239ef14b6f5123d173e4d3d0
marcelvanherk <marcelvanherk@users.noreply.github.com>
2024-09-27 20:31:08 +0100

    Removed cgi-bin and node_qido_wado web folders

--- webserver/cgi-bin/OpenClinica/SubmitStatus.lua
+++
@@ -1,5 +0,0 @@
--- test version 20120719
-
-HTML('Content-type: text/plain\n\n');
-print(servercommand('status_string:'));
-
--- webserver/node_qido_wado/app/plugins/services.js
+++
@@ -1,4 +0,0 @@
-import DicomService from '~/services/DicomService'
-export default ({ $axios }, inject) => {
-  inject('DicomService', DicomService($axios))
-}

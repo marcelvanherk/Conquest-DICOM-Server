marcelvanherk/Conquest-DICOM-Server

commit aa71ea5018fe12e20df8c64dac19a41ce30135e9

Fix rare Json formatting error in DS
20260813	mvh	Fix json formating of DS like 0600,-0600

src/dgate/src/dgate.cpp
@@ -1249,6 +1249,7 @@ Spectra0013 Wed, 5 Feb 2014 16:57:49 -0200: Fix cppcheck bugs #8 e #9
 20260716	mvh	Emit boundary at start of each dicom object; re-added \r \n translation by Divinis
 20260809   	mvh	Catch errors of dbase.CreateTable (crashed server browser for mixed users)
 20260813	mvh	Extend Lua package.path for require() to search in server/lua folder
+20260813	mvh	Fix json formating of DS like 0600,-0600
 
 ENDOFUPDATEHISTORY
 */
@@ -8031,7 +8032,7 @@ static ExtendedPDU_Service ScriptForwardPDU[1][MAXExportConverters];	// max 20*2
 	         p=strchr(p+1, '\\');
 		 if (p) *p=0;
 		 if (*q == '+') q++;
-		 Index+=sprintf(result+Index, "%s,", q);
+		 Index+=sprintf(result+Index, "%d,", atoi(q));
 		 count1++;
 	      }
 	      if (count1) Index--;

commit ef9d4f632eb318152329c62e61e47509a2e96c09

Fixed dicomweb formatting of DS items, broken with aa71ea5
20260818	mvh	Fixed above fix that broke formatting of float values

Test case:

> a=DicomObject:new() a.ImagePosition=[[-0060\0.5\1e2\+020\+0015e1]];print(a:Serialize(true, false, true))
[{"00200030": { "vr": "DS", "Value":[-60,0.500000,100.000000,20,150.000000]}}]

src/dgate/src/dgate.cpp
@@ -1249,7 +1249,8 @@ Spectra0013 Wed, 5 Feb 2014 16:57:49 -0200: Fix cppcheck bugs #8 e #9
 20260716	mvh	Emit boundary at start of each dicom object; re-added \r \n translation by Divinis
 20260809   	mvh	Catch errors of dbase.CreateTable (crashed server browser for mixed users)
 20260813	mvh	Extend Lua package.path for require() to search in server/lua folder
-20260813	mvh	Fix json formating of DS like 0600,-0600
+20260813	mvh	Fix json formatting of DS like 0600,-0600
+20260818	mvh	Fixed above fix that broke formatting of float values
 
 ENDOFUPDATEHISTORY
 */
@@ -8032,7 +8033,10 @@ static ExtendedPDU_Service ScriptForwardPDU[1][MAXExportConverters];	// max 20*2
 	         p=strchr(p+1, '\\');
 		 if (p) *p=0;
 		 if (*q == '+') q++;
-		 Index+=sprintf(result+Index, "%d,", atoi(q));
+		 if (strchr(q, '.') || strchr(q, 'e') || strchr(q, 'E'))
+		   Index+=sprintf(result+Index, "%f,", atof(q));
+	         else
+		   Index+=sprintf(result+Index, "%d,", atoi(q));
 		 count1++;
 	      }
 	      if (count1) Index--;